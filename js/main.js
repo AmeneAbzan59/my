05f48cc866178f7fbb43c0790bf9c7032137f11b
AmeneAbzan <ameneabzan59@gmail.com>
2024-11-15 17:22:01 +0330

    slider

---
+++ js/main.js
@@ -0,0 +1,71 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 12.5,
+  spaceBetween: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".mySlider", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".myOffer", {
+  slidesPerView: 8.1,
+  spaceBetween: 3,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".myMostPopular", {
+  slidesPerView: 9.3,
+  spaceBetween: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".myFavorite", {
+  slidesPerView: 4,
+  spaceBetween: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".myBest-Seller", {
+  slidesPerView: 4,
+  spaceBetween: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+var swiper = new Swiper(".mySelect", {
+  slidesPerView: 6,
+  spaceBetween: 3,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
